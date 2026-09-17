267ca8db294e5f5f0e917763fbcb804bc09ac212
Abu Bakar <abubakar4232h@gmail.com>
2026-09-17 12:15:55 +0530

    add user

--- LAB4/prg7.js
+++ LAB4/prg7.js
@@ -1,25 +1,67 @@
 import http from "http";
-import { getAllUsers } from "./users.js";
+import { getUsers, addUser } from "./users.js";
+
 const server = http.createServer((req, res) => {
-  if ((req.url === "/api/users" && req.method=="GET")){
-     res.end(JSON.stringify({msg: "add user"}));
-} 
-else if (req.url === "/api/users" && req.method=="POST") {
-    res.end(JSON.stringify({msg: "add user"}));
+
+  if (req.url === "/api/users" && req.method === "GET") {
+
+    res.end(JSON.stringify(getUsers()));
+
   } 
-  else if (req.url === "/api/users" && req.method=="GET") {
-    res.end(JSON.stringify({msg: "single user with id 1"}));
+  
+  else if (req.url === "/api/users" && req.method === "POST") {
+
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+
+      const user = JSON.parse(body);
+
+      const userCreated = addUser(user);
+
+      res.end(JSON.stringify({
+        msg: "user added",
+        userCreated
+      }));
+
+    });
+
   } 
-  else if(req.url === "/api/users" && req.method=="PUT") {
-    res.end(JSON.stringify({msg: "update user with id 1"}));
-  }
-   else if(req.url === "/api/users" && req.method=="DELETE") {
-    res.end(JSON.stringify({msg: "delete user with id 1"}));
-  }
-  else{
-    res.statusCode= 404;
+  
+  else if (req.url === "/api/users/1" && req.method === "GET") {
+
+    res.end(JSON.stringify({
+      msg: "single user with id 1"
+    }));
+
+  } 
+  
+  else if (req.url === "/api/users/1" && req.method === "PUT") {
+
+    res.end(JSON.stringify({
+      msg: "update user 1"
+    }));
+
+  } 
+  
+  else if (req.url === "/api/users/1" && req.method === "DELETE") {
+
+    res.end(JSON.stringify({
+      msg: "remove 1"
+    }));
+
+  } 
+  
+  else {
+
+    res.statusCode = 404;
     res.end();
+
   }
 });
 
-server.listen(3001, () => console.log("prg7 is running..."));
+server.listen(3001, () => console.log("prg7 is running"));
--- LAB4/users.js
+++ LAB4/users.js
@@ -1,8 +1,25 @@
-// we use in memory data base
-let user = [
-    {id:1 , name:"abu" , mob:"708090100" , email:"abu@gmail.com"},
-    {id:2 , name:"zaid" , mob:"45673829" , email:"zaid@gmail.com"},
-]
+// we use in memory database
+let users = [
+  {
+    id: 1,
+    name: "Abu",
+    mob: "98345xxxxx",
+    email: "abu.example@exam.com",
+  },
+  {
+    id: 2,
+    name: "zaid",
+    mob: "92345xxxxx",
+    email: "zaid.example@exam.com",
+  },
+];
+
 let nextId = 3;
 
-export const getAllUsers = (req, res) => users ; 
+export const getUsers = () => users;
+
+export const addUser = (user) => {
+  user.id = nextId++ ; 
+  users.push(user) ; 
+  return user ; 
+}
